11268f84e759e9c223495d8af3ca05305266ceab
Rafael <rhmcord@gmail.com>
2025-06-11 10:45:51 -0500

    feat: update post

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
-app.post("/dicom/ping", (req, res) => {
+app.post("/dicom/ping", express.json(), async (req, res) => {
   const { ip, port, aet_server, aet_client = "MY_CLIENT_AE" } = req.body;
 
   if (!ip || !port || !aet_server) {
@@ -17,20 +17,16 @@ app.post("/dicom/ping", (req, res) => {
       .json({ ok: false, error: "Missing required fields" });
   }
 
-  const echoScu = new dimse.CEchoSCU();
-  echoScu
-    .start({
-      callingAETitle: aet_client,
-      calledAETitle: aet_server,
-      host: ip,
-      port: Number(port),
-    })
-    .then(() => {
-      res.json({ ok: true, status: true });
-    })
-    .catch((err) => {
-      res.status(500).json({ ok: false, error: err.message });
+  try {
+    await dimse.CEchoSCU({
+      source: { aet: aet_client },
+      target: { host: ip, port, aet: aet_server },
     });
+
+    res.json({ ok: true, status: true });
+  } catch (err) {
+    res.status(500).json({ ok: false, error: err.message });
+  }
 });
 
 app.get("/", (_, res) => {
